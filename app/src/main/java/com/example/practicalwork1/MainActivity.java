cb97f6e1abe7351463af5ace4412baf13fc9f474
Ivan Dobrokhvalov <dobrokhvalov.official@ya.ru>
2023-02-16 14:49:00 +0300

    Initial commit

---
+++ app/src/main/java/com/example/practicalwork1/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.practicalwork1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = this.getClass().getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
