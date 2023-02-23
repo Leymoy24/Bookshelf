65ec2d040475861bf5a4be47062fcbf53c794048
Ivan Dobrokhvalov <dobrokhvalov.official@ya.ru>
2023-02-23 19:11:31 +0300

    Adding Event Handlers

--- app/src/main/java/com/example/practicalwork1/MainActivity.java
+++ app/src/main/java/com/example/practicalwork1/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,13 +14,15 @@ import com.example.practicalwork1.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String TAG = this.getClass().getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // Инициализацию TextView строковым ресурсом с помощью ViewBinding
+        // Инициализация TextView строковым ресурсом с помощью ViewBinding
         TextView textViewBookshelf = binding.mainActivityTextViewBookshelf;
         String textBookshelf = getString(R.string.welcome_to_bookshelf);
         textViewBookshelf.setText(textBookshelf);
@@ -28,5 +31,19 @@ public class MainActivity extends AppCompatActivity {
         ImageView imageViewBook1 = binding.mainActivityBookPicture;
         Drawable drawableImageBook1 = getDrawable(R.drawable.book_5);
         imageViewBook1.setImageDrawable(drawableImageBook1);
+
+        // Программное задание обработчика событий
+        binding.button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Click go to profile");
+            }
+        });
     }
+
+    // Декларативное задание метода обработчика событий
+    public void buttonSaveClickHandler(View view) {
+        Log.d(TAG, "Click Save");
+    }
+
 }
