faacb8ebdd9de4d29da0f9ec1bd045a9860b23b4
Ivan Dobrokhvalov <dobrokhvalov.official@ya.ru>
2023-02-23 17:31:00 +0300

    Initialization of components in the layout with data from the program code using ViewBinding

--- app/src/main/java/com/example/practicalwork1/MainActivity.java
+++ app/src/main/java/com/example/practicalwork1/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.practicalwork1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.practicalwork1.databinding.ActivityMainBinding;
@@ -15,5 +18,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        // Инициализацию TextView строковым ресурсом с помощью ViewBinding
+        TextView textViewBookshelf = binding.mainActivityTextViewBookshelf;
+        String textBookshelf = getString(R.string.welcome_to_bookshelf);
+        textViewBookshelf.setText(textBookshelf);
+
+        // Инициализация ImageView картинкой с помощью ViewBinding
+        ImageView imageViewBook1 = binding.mainActivityBookPicture;
+        Drawable drawableImageBook1 = getDrawable(R.drawable.book_5);
+        imageViewBook1.setImageDrawable(drawableImageBook1);
     }
 }
